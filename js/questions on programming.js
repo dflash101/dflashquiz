f6fc2c5f2c4d030ffd78a5e6a3f8406e1ceb5925
dflash101 <115047230+dflash101@users.noreply.github.com>
2025-03-18 00:13:17 +0000

    Add files via upload

---
+++ js/questions on programming.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who is the father of C language??",
+    answer: "Dennis Ritchie",
+    options: [
+      "Steve Jobs",
+      "James Gosling",
+      "Charles Babbage",
+      "Dennis Ritchie"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is not a valid C variable name?",
+    answer: "int $main;",
+    options: [
+      "int number;",
+      "float rate;",
+      "int variable_count;",
+      "int $main;"
+    ]
+  },
+    {
+    numb: 3,
+    question: " All keywords in C are in ____________",
+    answer: "LowerCase letters",
+    options: [
+      "LowerCase letters",
+      "UpperCase letters",
+      "CamelCase letters",
+      "None of the mentioned"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following cannot be a variable",
+    answer: "volatile",
+    options: [
+      "export",
+      "friend",
+      "volatile",
+      "true"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which keyword is used to prevent any changes in the variable within a C program?",
+    answer: "const",
+    options: [
+      "immutable",
+      "const",
+      "volatile",
+      "mutable"
+    ]
+  },
+ 
+];
